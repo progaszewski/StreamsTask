1cad0f58e48759419983bc8150fc383a582c72c6
progaszewski <piotr.9@wp.pl>
2014-06-17 17:37:12 +0200

    usuniecie niepotrzebnego importu

--- src/services/CustomerService.java
+++ src/services/CustomerService.java
@@ -4,7 +4,6 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.OptionalDouble;
 import java.util.stream.Collectors;
